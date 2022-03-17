b030f6d6a45d7ee8efaa193d888eec3fcef74386
Dennis Wanyoike <dwanyoike@codedcell.com>
2022-03-17 08:00:34 +0300

    Prevent FlatPickr From Triggering changes all the time

--- src/resources/views/components/date.blade.php
+++ src/resources/views/components/date.blade.php
@@ -24,15 +24,15 @@ use Illuminate\Support\Str;
         <input @class([
             'block w-full sm:text-sm rounded-md pr-10',
             'shadow-sm border-gray-300 focus:ring-indigo-500 focus:border-indigo-500' => !$errors->has(
-                $name,
+                $name
             ),
             'pr-10 border-red-300 text-red-900 placeholder-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 ' => $errors->has(
-                $name,
+                $name
             ),
         ]) type="text" name="{{ $name }}" id="{{ $name }}" wire:ignore
             x-data="{value: @entangle($attributes->wire('model')), instance: undefined}" x-init="() => {
-                $watch('value', value => instance.setDate(value, true));
-                instance = flatpickr($refs.input, {{ json_encode((object) $options) }});console.log(value)
+                $watch('value', value => instance.setDate(value, false));
+                instance = flatpickr($refs.input, {{ json_encode((object) $options) }});
             }" x-ref="input" x-bind:value="value" {{ $attributes->merge([]) }} placeholder="{{ $placeholder }}"
             aria-describedby="{{ $name }}-description" />
         <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
